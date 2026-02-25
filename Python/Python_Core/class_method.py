681c61822d82ccb420437b9adb29c37ca0819d3e
Vaishnavi Aluvoju <aluvojuvaishnavi@gmail.com>
2026-02-25 09:52:06 +0530

    commit

---
+++ Python/Python_Core/class_method.py
@@ -0,0 +1,17 @@
+class Profile:
+    def __init__(self,username):
+        self.followers=0
+        self.username=username
+    def follow(self):
+        print("someone started following you")
+        self.followers+=1
+    def edit(self,a):
+        self.username=a
+        print("Username changed")
+p1=Profile("_mshr_ak")
+p1.follow()
+print(p1.username)
+print(p1.followers)
+p1.edit("vaishu_13")
+print(p1.username)
+print(p1.followers)
