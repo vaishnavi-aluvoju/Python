fe51793501e0896a9186e9e594172a14b75bfd6f
Vaishnavi Aluvoju <aluvojuvaishnavi@gmail.com>
2026-02-26 09:57:09 +0530

    commit

--- Python/Python_Core/calss_building.py
+++ Python/Python_Core/calss_building.py
@@ -5,6 +5,8 @@ class building:
         self.name=name
         self.wifi=wifi
         building.total_buildings+=1
+    def change_rooms(self,nor):
+        building.no_of_rooms=nor
 b1=building("xyz",1)
 print(b1.name)
 print(b1.wifi)
@@ -14,4 +16,8 @@ b2=building("vaishu's_palace",2)
 print(b2.name)
 print(b2.wifi)
 print(b2.total_buildings)
+b3=building("ygf",0)
+b3.change_rooms(20)
+print(b3.no_of_rooms)
+print(building.no_of_rooms)
 
